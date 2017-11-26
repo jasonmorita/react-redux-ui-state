4af762a54d9544cb5cb32f09981696200fdbcfe3
Jason Morita <jason@blst.com>
2017-11-25 22:26:45 -0800

    Adds more examples

--- example/src/App.js
+++ example/src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { uiState } from "react-redux-ui-state";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./Button";
+import Text from "./Text";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
-            </div>
-        );
-    }
+function App({ setUiState, uiAppFlag }) {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+                <button onClick={() => setUiState({ uiAppFlag: !uiAppFlag })}>
+                    Toggle `uiAppFlag` ( {uiAppFlag.toString()})
+                </button>
+            </p>
+            <Button />
+            <Text />
+        </div>
+    );
 }
+
 const uiStateConfig = {
-    name: "Example",
+    name: "AppMain",
     state: () => ({
-        uiExample: true,
+        uiAppFlag: true,
     }),
 };
 
